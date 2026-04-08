6471711502fb0511c2795e2da9af792447168d20
ramalakshmi0304 <mlaksmipradeep@gmail.com>
2026-04-08 14:45:31 +0530

    auth middleware updated

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -5,7 +5,6 @@ import User from '../models/User.js';
 export const protect = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  // Ensure header exists and starts with "Bearer "
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     console.log('AUTH FAIL: No Bearer token in Authorization header');
     return res.status(401).json({ message: 'Not authorized, no token' });
@@ -14,12 +13,12 @@ export const protect = async (req, res, next) => {
   const token = authHeader.split(' ')[1];
 
   try {
-  // Replace process.env.JWT_SECRET with the ACTUAL string for one test
-  const decoded = jwt.verify(token, "a-string-secret-at-least-256-bits-long"); 
-  req.user = await User.findById(decoded.id).select('-password');
-  next();
-} catch (error) {
-  console.log('JWT Error:', error.message);
-  res.status(401).json({ message: 'Not authorized' });
-}
+   const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decoded.id).select('-password');
+    next();
+  } catch (error) {
+    console.log('JWT Error:', error.message);
+    res.status(401).json({ message: 'Not authorized' });
+  }
 };
